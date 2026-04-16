8153d29d5eb0d1e515b7efc1a9a86c76d7b43ba3
Y. H. Tsang <138969757+Y-Tsang@users.noreply.github.com>
2026-04-16 19:55:05 +0800

    1

---
+++ src/data/bio.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Link from '../components/ui/Link';
+
+export const bioText = (
+  <>
+    I am a Master's student in the <Link link="https://cse.hkust.edu.hk/">Department of Computer Science and Engineering</Link> at <Link link="https://hkust.edu.hk/">HKUST</Link>. Before that, I did my undergraduate studies in the <Link link="https://www.cs.cityu.edu.hk/">Department of Computer Science</Link> at <Link link="https://www.cityu.edu.hk/">CityUHK</Link>, where I was supervised by Prof. <Link link="https://shengdongzhao.com/">Shengdong Zhao</Link>. My research envisions an intent-centered society where technologies seamlessly integrate into daily life to precisely support our needs. To realize this, my work explores strategies that enhance both human-to-human and human-AI collaboration. Specifically, my past projects include advancing the use of generative AI in argumentative discourse, designing visual cues that enrich the expression of intent, and developing a digitally co-present AR social platform for outdoor activities.
+  </>
+);
